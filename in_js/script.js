ebc92c519d29740fec18aca8434de410f26479c5
scorcism <abhishekpathak1720@gmail.com>
2023-02-28 17:04:59 +0530

    In JS chnages

--- in_js/script.js
+++ in_js/script.js
@@ -51,8 +51,9 @@ head_container__2_ul.appendChild(head_ul__li1)
 let head_ul__li2 = document.createElement('li')
 head_ul__li2.classList.add('head_ul__li')
 let a2 = document.createElement('a')
-let a_text2 = document.createTextNode('About us')
-a2.href = "#"
+let a_text2 = document.createTextNode('About')
+a2.href = "https://github.com/scorcism"
+a2.target = "_blank"
 a2.appendChild(a_text2)
 head_ul__li2.appendChild(a2)
 head_container__2_ul.appendChild(head_ul__li2)
@@ -72,7 +73,7 @@ let head_ul__li4 = document.createElement('li')
 head_ul__li4.classList.add('head_ul__li')
 let a4 = document.createElement('a')
 let a_text4 = document.createTextNode('Contribute')
-a4.href = "#"
+a4.href = "https://github.com/scorcism/website-using-js/"
 a4.appendChild(a_text4)
 head_ul__li4.appendChild(a4)
 head_container__2_ul.appendChild(head_ul__li4)
@@ -81,8 +82,12 @@ head_container__2_ul.appendChild(head_ul__li4)
 let head_ul__li5 = document.createElement('li')
 head_ul__li5.classList.add('head_ul__li')
 let button1 = document.createElement('button')
-button1.textContent = "To Moon"
 button1.classList.add('head_ul__li_button')
+let aa = document.createElement('a')
+aa.innerText = "To Moon"
+aa.href= "https://theuselessweb.com/"
+aa.target = "_blank"
+button1.appendChild(aa)
 head_ul__li5.appendChild(button1)
 head_container__2_ul.appendChild(head_ul__li5)
 
@@ -94,14 +99,14 @@ companey_left.classList.add('companey_left')
 about_companey.appendChild(companey_left)
 let c_h1 = document.createElement('h1')
 c_h1.classList.add('c_h1')
-c_h1.innerText = " Lorem ipsum dolor sit amet consectetur adipisicing."
+c_h1.innerText = "Website Made with HTML, CSS"
 companey_left.appendChild(c_h1)
 let c_hr = document.createElement('hr')
 c_hr.classList.add('c_hr')
 companey_left.appendChild(c_hr)
 root.appendChild(about_companey)
 let c_p = document.createElement('p')
-c_p.innerText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae quidem sit, eligendi sequi placeat sed?`
+c_p.innerText = `This website is entirely made using HTML,CSS. This is an HTML preview which is converted to JS`
 companey_left.appendChild(c_p)
 let companey_right = document.createElement('div')
 companey_right.classList.add('companey_right')
@@ -119,13 +124,14 @@ cta_text.classList.add('cta_text')
 cta_container.appendChild(cta_text)
 let h3 = document.createElement('h3')
 h3.classList.add("h3");
-h3.innerText = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate."
+h3.innerText = " In order to be irreplaceable, one must always be different"
 cta_text.appendChild(h3)
 let cta_button = document.createElement('div')
 cta_button.classList.add('cta_button')
 let a  = document.createElement('a')
 a.innerText = "To Moon"
-a.href = "#"
+a.href = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGIWnQUKyw86jU3eiDBtYXEKD5Fmy0W-7cXCWOjzMbw&s"
+a.target = "_blank"
 cta_button.appendChild(a)
 cta_container.appendChild(cta_button)
 root.appendChild(cta)
